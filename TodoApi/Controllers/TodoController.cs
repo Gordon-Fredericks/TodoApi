50d6cd653449fc643ab647e55bcc6b6e3e5b2e04
gordonf <gfredericks@trailappliances.com>
2018-04-30 16:29:47 -0700

    fixed ActionResult errors
    - submitted GitHub feedback:
    https://github.com/aspnet/Docs/issues/6145

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -6,7 +6,7 @@ using TodoApi.Models;
 namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    //[ApiController]
     public class TodoController : ControllerBase
     {
         private readonly TodoContext _context;
@@ -21,5 +21,48 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return new ObjectResult(_context.TodoItems.ToList());
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        //[HttpGet]
+        //public ActionResult<List<TodoItem>> GetAll()
+        //{
+        //    return _context.TodoItems.ToList();
+        //}
+
+        //[HttpGet("{id}", Name = "GetTodo")]
+        //public ActionResult<TodoItem> GetById(long id)
+        //{
+        //    var item = _context.TodoItems.Find(id);
+        //    if (item == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return item;
+        //}
     }
 }
